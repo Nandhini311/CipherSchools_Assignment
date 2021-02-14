f2ba1e838df7d6635353bffa0961bec49cd79228
ibuttercup <63045112+Nandhini311@users.noreply.github.com>
2021-02-14 07:41:12 +0530

    Create 3 Finding Peak Element.cpp

---
+++ 1.Searching & Sorting (Feb 8)/3 Finding Peak Element.cpp
@@ -0,0 +1,55 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+
+//naive solution
+int Peakelement(int arr[], int  n)
+{
+    if(n==1) return arr[0];
+    if(arr[0]>=arr[1]) return arr[0];
+    if(arr[n-1]>=arr[n-2]) return arr[n-1];  //checking if the corner elements are peak elements
+    for(int i=1;i<n-1;n++)
+    {
+        if(arr[i]>=arr[i-1] && arr[i]<=arr[i+1])
+           return arr[i];
+    }
+}
+
+//efficient solution
+int Peakelementefficient(int arr[], int n)
+{
+    int low = 0, high = n-1;
+    while(low<=high)
+    {
+        int mid = (low+high)/2;
+        if((mid == 0 || arr[mid-1] <= arr[mid]) &&           //if there is any neighbor element, it sees if its an peak element
+           (mid == n-1 || arr[mid+1] <= arr[mid]))
+           {
+               return mid;
+           }
+        if(mid>0 && arr[mid-1]<=arr[mid])
+          low = mid+1;
+        else
+          high = mid-1;
+    }
+}
+
+
+
+int main()
+{
+    int arr[] = {10, 2, 4, 8, 3, 9, 30};
+    cout<<Peakelement(arr, 7);
+    cout<<endl;
+    cout<<Peakelementefficient(arr, 7); //returns the peak elements position
+    return 0;
+}
