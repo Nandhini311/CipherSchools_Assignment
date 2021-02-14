cd25bf162c4f7d5fda84d0f54dbe7d8c154d59e0
ibuttercup <63045112+Nandhini311@users.noreply.github.com>
2021-02-14 17:48:38 +0530

    Update 8 Alternative Sorting.cpp

--- 1.Searching & Sorting (Feb 8)/8 Alternative Sorting.cpp
+++ 1.Searching & Sorting (Feb 8)/8 Alternative Sorting.cpp
@@ -1,29 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int* alternatesorting(int arr[],int n)
+void alternatesorting(int arr[],int n)
 {
     
      sort(arr,arr+n);
      int i=0, j=n-1;
      while(i<j){
-         cout<,arr[j--];
+         cout<<arr[j--];
          cout<<arr[i++];
      }
      if(n%2 !=0)
      cout<<arr[i];
 }
-void print(int arr[], int n)
-{
-    for(int i=0;i<n;i++)
-     {
-         cout<<arr[i];
-     }
-}
+
 int main()
 {
-   int arr[]={0,1,2,0,1,2,1,0,2,0,1,1};
-   alternatesorting(arr,12);
-   print(arr,12);
+   int arr[]={8,3,4,2,9,3,1,2};
+   alternatesorting(arr,8);
     return 0;
 }
