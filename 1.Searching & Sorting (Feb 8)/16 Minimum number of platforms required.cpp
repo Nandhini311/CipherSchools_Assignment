b9230450ff0058393d737375c9ce8450f558dbbb
ibuttercup <63045112+Nandhini311@users.noreply.github.com>
2021-02-16 01:22:51 +0530

    Create 16 Minimum number of platforms required.cpp

---
+++ 1.Searching & Sorting (Feb 8)/16 Minimum number of platforms required.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int findPlatform(int arr[], int dep[], int n)
+{
+    int plat_needed = 1, result = 1;
+    int i = 1, j = 0;
+ 
+    for (int i = 0; i < n; i++) {
+        plat_needed = 1;
+ 
+        for (int j = i + 1; j < n; j++) {
+            // check for overlap
+            if ((arr[i] >= arr[j] && arr[i] <= dep[j]) || 
+           (arr[j] >= arr[i] && arr[j] <= dep[i]))
+                plat_needed++;
+        }
+ 
+        result = max(result, plat_needed);
+    }
+ 
+    return result;
+}
+ 
