b3fa912e83234fa7b63ebe641d029c19ca874fff
ibuttercup <63045112+Nandhini311@users.noreply.github.com>
2021-02-16 07:05:17 +0530

    Create 15 Largest sum contiguous subarray.cpp

---
+++ 1.Searching & Sorting (Feb 8)/15 Largest sum contiguous subarray.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int MaxSubarray(int arr[], int n)
+{
+    int res = 0;
+	int Max = arr[0];
+	for(int i=1;i<n;i++)
+	{
+	    Max = max(Max+arr[i], arr[i]);
+	    res = max(Max, res);
+	}
+	return res;
+}
+
+int main() {
+	
+     int arr[] = {5, 0, 6, 2, 3}, n = 5;
+     cout<<MaxSubarray(arr, n);
+    
+}
