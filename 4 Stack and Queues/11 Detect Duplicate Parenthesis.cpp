202d122b8893a43b2f198afe61f161172c2cd15e
ibuttercup <63045112+Nandhini311@users.noreply.github.com>
2021-02-20 22:18:25 +0530

    Create 11 Detect Duplicate Parenthesis.cpp

---
+++ 4 Stack and Queues/11 Detect Duplicate Parenthesis.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+bool findDuplicateparenthesis(string str) 
+{ 
+	stack<char> Stack;  
+	for (char ch : str) 
+	{
+		if (ch == ')') 
+		{ 
+	
+			char top = Stack.top(); 
+			Stack.pop(); 
+
+			int elementsInside = 0; 
+			while (top != '(') 
+			{ 
+				elementsInside++; 
+				top = Stack.top(); 
+				Stack.pop(); 
+			} 
+			if(elementsInside < 1) { 
+				return 1; 
+			} 
+		} 
+		else
+			Stack.push(ch); 
+	} 
+ 
+	return false; 
+} 
+
+
+int main() 
+{ 
+	string str = "(((a+(b)))"; 
+
+	if (Duplicateparenthesis(str)) 
+		cout << "Duplicate Found "; 
+	else
+		cout << "No Duplicates Found "; 
+
+	return 0; 
+} 
